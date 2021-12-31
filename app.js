8d88cb87fb5bb440d0219dba7d271e1fc9203529
Tamago <yukotaniguchi@outlook.com>
2022-01-01 09:56:55 +1100

    large screen added

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const shareBtn=document.querySelector("#shareBtn");
 const shareBrnDark=document.querySelector("#shareBtnDark")
+const shareBtnLg=document.querySelector("#shareBtn-lg");
+const shareBrnDarkLg=document.querySelector("#shareBtnDark-lg")
 
 shareBtn.addEventListener('click', (e)=>{
     const author=document.querySelector("#author");
@@ -18,3 +20,30 @@ shareBtnDark.addEventListener('click', (e)=>{
     author.style.display="flex";
     footer.style.backgroundColor="#fff";
 })
+
+shareBtnLg.addEventListener('click', (e)=>{
+    const share=document.querySelector("#share");
+    const newSquare=document.createElement('div');
+    const shareBtnLgOff=document.createElement('a');
+    const author=document.querySelector("#author");
+    share.appendChild(newSquare);
+    newSquare.classList.add('square');
+    share.classList.add('bubble');
+    shareBtnLg.remove();
+    author.appendChild(shareBtnLgOff);
+    shareBtnLgOff.classList.add("fa");
+    shareBtnLgOff.classList.add("fa-share");
+
+        shareBtnLgOff.addEventListener('click',(e)=>{
+            newSquare.remove();
+            newSquare.classList.remove('square');
+            share.classList.remove('bubble');
+            shareBtnLgOff.remove();
+            author.appendChild(shareBtnLg);
+})
+
+});
+
+
+
+
